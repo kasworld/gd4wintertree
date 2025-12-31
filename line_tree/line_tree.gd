cec713cdac7b33b648d26ac237fcc445795df0ea
seukwonkang <kasworld@gmail.com>
2025-12-31 11:56:48 +0900

    adjust count param

--- line_tree/line_tree.gd
+++ line_tree/line_tree.gd
@@ -11,17 +11,16 @@ func init(h :float, w :float, co1 :Color, co2 :Color, line_width :float = 0.5, s
 	for y in range(h,0,-1):
 		var rate := (h-y)/h
 		var r :float= lerp(1.0, w/2, rate ) + fposmod(rate*h, h/stage_count)*rate
-		lines.append_array(make_lines(y,w*2*rate,10*rate,r))
+		lines.append_array(make_lines(y,w*1.5*(rate+0.1),10*rate,r))
 	$Lines.multi_line_by_pos(lines, line_width, Color.WHITE)
 	$Lines.set_gradient_color_all(co1,co2)
 	return self
 
 func make_lines(start_y :float, count :int, h :float, r :float) -> Array:
-	count = max(count,3)
 	var rtn := []
 	var rad_step := 2*PI / count
 	var end_y := start_y+h
-	var rad_shift :float = randf_range(0,2*PI)
+	var rad_shift :float = randf_range(0,rad_step)
 	for i in count:
 		var rad := rad_step*i + rad_shift
 		var to := Vector3(cos(rad)*r, start_y  , sin(rad)*r)
