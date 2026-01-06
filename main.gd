cfeb0045d3f0c12fcd44e9d647834443805e6b18
seukwonkang <kasworld@gmail.com>
2026-01-06 16:28:15 +0900

    rename to winter tree with 장식들

--- main.gd
+++ main.gd
@@ -48,82 +48,61 @@ func _ready() -> void:
 	$FixedCameraLight.make_current()
 	$GlassCabinet.init(WorldSize)
 
-	var bmesh := PrismMesh.new()
-	bmesh.size = Vector3(1,0.3,1)
-	line_tree = preload("res://line_tree/line_tree.tscn").instantiate(
-		).init(bmesh, WorldSize.y, WorldSize.z/2, WorldSize.y*2, PI, 1.0,
+	winter_tree = preload("res://winter_tree/winter_tree.tscn").instantiate(
+		).init(WorldSize.y, WorldSize.z/2, WorldSize.y*2, PI, 1.0,
 		).set_center_color(Color.GREEN)
-	add_child(line_tree)
-	$"왼쪽패널/LabelTree".text = "branch count %d" % [ line_tree.get_lines().multimesh.instance_count ]
-	line_tree.position.y = - WorldSize.y/2
-	line_tree_inst_index = line_tree.make_index_array()
+	add_child(winter_tree)
+	$"왼쪽패널/LabelTree".text = "branch count %d" % [ winter_tree.가지들얻기().multimesh.instance_count ]
+	winter_tree.position.y = - WorldSize.y/2
+	winter_tree_inst_index = winter_tree.make_index_array()
 
-	var sp_mesh := SphereMesh.new()
-	sp_mesh.radius = 0.5
-	sp_mesh.height = 0.5*2
-
-	#mesh_tree = preload("res://mesh_tree/mesh_tree.tscn").instantiate(
-		#).init(sp_mesh, WorldSize.y, WorldSize.z/2, WorldSize.y/5, PI/5)
-
-	var pos_list := []
-	for i in 1000:
-		var l = line_tree.line_list.pick_random()
-		pos_list.append(l[1])
-	mesh_tree = preload("res://multi_mesh_shape/multi_mesh_shape.tscn").instantiate(
-		).init_meshs_by_point_list(sp_mesh, pos_list, Color.WHITE)
-	add_child(mesh_tree)
-	mesh_tree.position.y = - WorldSize.y/2
-
-#var mesh_tree :MeshTree
-var mesh_tree :MultiMeshShape
-var line_tree :LineTree
+var winter_tree :WinterTree
 enum AniDir { Up, Down, Left , Right }
-var line_tree_inst_index :Array
+var winter_tree_inst_index :Array
 var color_fn_args := ShuffleIter.new( [[0],[1],[2],[0,1],[1,2],[2,0], [0,1,2]] )
 var color_fn :Callable = RandomColor.pure_color
 var ani_dir_data := ShuffleIter.new( [AniDir.Up, AniDir.Down, AniDir.Left , AniDir.Right] )
 var change_count := 0
 func linetree_animate(delta :float) -> void:
-	mesh_tree.rotate_y(delta)
-	line_tree.rotate_y(delta)
-	var lines :MultiMeshShape = line_tree.get_lines()
+	winter_tree.rotate_y(delta)
+	var lines :MultiMeshShape = winter_tree.가지들얻기()
 	var co :Color = color_fn.call(color_fn_args.get_current())
 	var ani_ended :bool = false
 	match ani_dir_data.get_current():
 		AniDir.Up:
-			for i in line_tree_inst_index[-change_count-1]:
+			for i in winter_tree_inst_index[-change_count-1]:
 				lines.set_inst_color(i, co)
 			change_count +=1
-			ani_ended = change_count >= line_tree_inst_index.size()
+			ani_ended = change_count >= winter_tree_inst_index.size()
 		AniDir.Down:
-			for i in line_tree_inst_index[change_count]:
+			for i in winter_tree_inst_index[change_count]:
 				lines.set_inst_color(i, co)
 			change_count +=1
-			ani_ended = change_count >= line_tree_inst_index.size()
+			ani_ended = change_count >= winter_tree_inst_index.size()
 		AniDir.Left:
-			for a :Array in line_tree_inst_index:
+			for a :Array in winter_tree_inst_index:
 				if change_count >= a.size():
 					continue
 				var i = a[change_count]
 				lines.set_inst_color(i, co)
 			change_count +=1
-			ani_ended = change_count >= line_tree_inst_index[-1].size()
+			ani_ended = change_count >= winter_tree_inst_index[-1].size()
 		AniDir.Right:
-			for a :Array in line_tree_inst_index:
+			for a :Array in winter_tree_inst_index:
 				if change_count >= a.size():
 					continue
 				var i = a[-change_count-1]
 				lines.set_inst_color(i, co)
 			change_count +=1
-			ani_ended = change_count >= line_tree_inst_index[-1].size()
-	mesh_tree.set_inst_color( randi_range(0, mesh_tree.multimesh.instance_count-1),  random_color())
+			ani_ended = change_count >= winter_tree_inst_index[-1].size()
+	winter_tree.장식들얻기().set_inst_color( randi_range(0, winter_tree.장식들얻기().multimesh.instance_count-1),  random_color())
 
 	if ani_ended:
 		color_fn_args.get_next()
 		ani_dir_data.get_next()
 		change_count = 0
 		color_fn = [RandomColor.pure_color, RandomColor.rate_color, random_color2].pick_random()
-		mesh_tree.set_color_all( random_color())
+		winter_tree.장식들얻기().set_color_all( random_color())
 
 var named_color_list := ShuffleIter.new(NamedColorList.color_list)
 func random_color2(_arg ) -> Color:
