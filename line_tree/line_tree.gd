6370a798499a954b017902cbb039f6098ff14cc5
seukwonkang <kasworld@gmail.com>
2026-01-06 13:17:07 +0900

    access line_list

--- line_tree/line_tree.gd
+++ line_tree/line_tree.gd
@@ -17,14 +17,15 @@ func init(mesh :Mesh, h :float, w :float, y_count :int, w_branch_density :float 
 	$"중심기둥".mesh.bottom_radius = w/100
 	$"중심기둥".mesh.height = h
 	$"중심기둥".position.y = h /2
-	var lines := []
+	line_list = []
+	instance_count_per_y = []
 	var y_step := h / y_count
 	for yi in range(y_count,0,-1):
 		var y := y_step * yi
 		var rate := 1.0 - y/h
 		var r :float= calc_radius_by_y(y, h, w, stage_count)
-		lines.append_array(make_lines(y, y_step, r * w_branch_density, 10*rate, r))
-	$Lines.multi_mesh_line_by_pos(mesh, lines, line_width, Color.WHITE)
+		line_list.append_array(make_lines(y, y_step, r * w_branch_density, 10*rate, r))
+	$Lines.multi_mesh_line_by_pos(mesh, line_list, line_width, Color.WHITE)
 	return self
 
 func calc_radius_by_y(y :float, h :float, w :float, stage_count :int) -> float:
@@ -44,7 +45,9 @@ func make_lines(start_y :float, y_step :float, count :int, h :float, r :float) -
 		rtn.append([Vector3(0, end_y+ y_shift, 0), to])
 	return rtn
 
-var instance_count_per_y :Array[int] = []
+var line_list :Array
+
+var instance_count_per_y :Array[int]
 func make_index_array() -> Array:
 	var rtn := []
 	var i := 0
